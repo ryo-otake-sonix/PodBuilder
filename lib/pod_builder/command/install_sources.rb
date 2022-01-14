7e01f50303f50a8e5f9d161a52aad05eae074316
rotake <rotake@sonix.asia>
2022-01-14 16:05:24 +0900

    Revert "test"
    This reverts commit acdf65db03ac8fbb9fb6207653ad9a87812d9fb8.

--- lib/pod_builder/command/install_sources.rb
+++ lib/pod_builder/command/install_sources.rb
@@ -50,8 +50,7 @@ module PodBuilder
         Pod::UI.puts "repo_dir: #{repo_dir}"
         Dir.chdir(dest_path) do
           Pod::UI.puts "current dir: #{Dir.pwd}"
-          Pod::UI.puts "対象ライブラリのリポジトリがあるかどうか: #{File.directory?(repo_dir)}"
-          Pod::UI.puts "リポジトリ一覧: #{`ls`}"
+          Pod::UI.puts "対象ライブラリのリポジトリがあるかどうか: #{!File.directory?(repo_dir)}"
           if !File.directory?(repo_dir) # 対象ライブラリのリポジトリがあるかどうか。なければcloneする。
             raise "\n\nFailed cloning #{spec.name}".red if !system("git clone #{spec.repo} #{spec.podspec_name}")
           end
